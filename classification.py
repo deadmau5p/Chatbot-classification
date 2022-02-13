a8237be7859b94c59c6e17bb05917af87aabfc28
Aljaz Potocnik <zajlap@gmail.com>
2022-02-13 13:10:34 +0100

    Data

---
+++ classification.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+data = pd.read_csv("./Sheet_1.csv")
+
+#data contains 80 user responses to a therapy chatbot. If a response is 'not flagged', the user can continue talking to the bot. 
+# If it is 'flagged', the user is referred to help
+#We classify new responses
+
+
+
